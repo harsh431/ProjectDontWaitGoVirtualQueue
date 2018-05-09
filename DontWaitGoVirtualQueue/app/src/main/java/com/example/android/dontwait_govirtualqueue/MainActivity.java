a08cdab9c90d8e9beec86fa4a8e9f191d1bce74a
harsh431 <harsh431@hotmail.com>
2018-05-09 14:14:15 +0530

    Project

---
+++ DontWaitGoVirtualQueue/app/src/main/java/com/example/android/dontwait_govirtualqueue/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.android.dontwait_govirtualqueue;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.login_button);
+        Button signUpButton = findViewById(R.id.signup_button);
+        loginButton.setOnClickListener(loginOnClickListener);
+        signUpButton.setOnClickListener(signUpOnClickListener);
+
+
+    }
+
+    private View.OnClickListener loginOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView loginsuccess = findViewById(R.id.login_successful_text);
+            loginsuccess.setVisibility(View.INVISIBLE);
+
+        EditText email = findViewById(R.id.email);
+        EditText pass = findViewById(R.id.password);
+        String result ="";
+        String[] userpass = {email.getText().toString(),pass.getText().toString()};
+        LoginValidator loginValidator = new LoginValidator();
+            try {
+                result = loginValidator.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,userpass).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            if(result.equals("valid"))
+            {
+                Log.v("harshdemo","valid");
+                TextView textView = findViewById(R.id.login_successful_text);
+                textView.setText("Login is successful!");
+                textView.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                Log.v("harshdemo","invalid");
+                TextView textView = findViewById(R.id.login_successful_text);
+                textView.setText("Sorry! Credentials are not valid!");
+                textView.setVisibility(View.VISIBLE);
+            }
+
+
+        }
+    };
+
+    public class LoginValidator extends AsyncTask<String,Void,String>
+    {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Socket s = new Socket("45.115.19.229",1234);
+                Log.v("harshdemo","Socket ");
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                dos.writeUTF("validate");
+                dos.writeUTF(strings[0]);
+                dos.writeUTF(strings[1]);
+                String result = dis.readUTF();
+                dos.close();
+                dis.close();
+                s.close();
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }
+    }
+
+    private View.OnClickListener signUpOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(getApplicationContext(),SignUp.class);
+            startActivity(intent);
+
+
+        }
+    };
+
+
+}
---
+++ DontWaitGoVirtualQueue/app/src/main/java/com/example/android/dontwait_govirtualqueue/SignUp.java
@@ -0,0 +1,108 @@
+package com.example.android.dontwait_govirtualqueue;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SignUp extends AppCompatActivity {
+
+    public class SignupDataStore extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... strings) {
+
+            try {
+                Socket s =new Socket("45.115.19.229",1234);
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                dos.writeUTF("store");
+                dos.writeUTF(strings[0]);
+                dos.writeUTF(strings[1]);
+                dos.writeUTF(strings[2]);
+                dos.writeUTF(strings[3]);
+                dos.close();
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Button signup = findViewById(R.id.signup_button);
+        signup.setOnClickListener(signupButtonClickListener);
+
+    }
+    private View.OnClickListener signupButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView textView = findViewById(R.id.signup_successful_text);
+            textView.setVisibility(View.INVISIBLE);
+            EditText firstname = findViewById(R.id.firstname);
+            EditText lastname = findViewById(R.id.lastname);
+            EditText email = findViewById(R.id.email);
+            EditText pass = findViewById(R.id.password);
+            EditText repass = findViewById(R.id.repassword);
+            TextView starfirstname = findViewById(R.id.starfirstname);
+            TextView starlastname = findViewById(R.id.starlastname);
+            TextView staremail = findViewById(R.id.staremail);
+            TextView starpass = findViewById(R.id.starpass);
+            TextView starrepass = findViewById(R.id.starrepass);
+            TextView[] stararray = {starfirstname,starlastname,staremail,starpass,starrepass};
+            EditText[] viewsarray = {firstname,lastname,email,pass,repass};
+            boolean fieldempty = false;
+            boolean passwordmismatch;
+            for(int i=0;i<viewsarray.length;i++)
+            {
+                if(viewsarray[i].getText().toString().equals(""))
+                {
+                    stararray[i].setVisibility(View.VISIBLE);
+                        fieldempty = true;
+
+                }
+                else
+                {
+                    stararray[i].setVisibility(View.INVISIBLE);
+                }
+            }
+            if(!pass.getText().toString().equals(repass.getText().toString()))
+            {
+                starrepass.setVisibility(View.VISIBLE);
+                passwordmismatch = true;
+            }
+            else
+            {
+                starrepass.setVisibility(View.INVISIBLE);
+                passwordmismatch = false;
+            }
+            if(!fieldempty && !passwordmismatch)
+            {
+                TextView textView1 = findViewById(R.id.signup_successful_text);
+                textView1.setVisibility(View.VISIBLE);
+                SignupDataStore sds = new SignupDataStore();
+                String[] data_to_store = {email.getText().toString(),pass.getText().toString(),firstname.getText().toString(),lastname.getText().toString()};
+                sds.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,data_to_store);
+            }
+
+        }
+    };
+
+}
